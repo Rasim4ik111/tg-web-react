3081fa7dd6634c10ab36ac144f75a1c3a9b95e1e
Mirrasim Suleymanli <rasim95suleymanli@gmail.com>
2023-10-20 16:56:47 +0400

    added useTelegram

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useEffect } from "react";
 import "./App.css";
-import { Button } from "./components/Button";
+import { Button } from "./components/Button/Button";
+import { useTelegram } from "./hooks/useTelegram";
 
 function App() {
+  const { onToggleButton, tg } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
   return (
     <>
-      <h1>Fuad burda qot vermek ucun elan yerlewirecek tezlikle gozleyin</h1>
-      <h1>
-        Ve elbetdeki yeni ulduzmuz Rasim qara bazara oz ekskuluziv sekiller ile
-        qelecek
-      </h1>
-      <Button />
+      <h1>Masa</h1>
+      <Button onClick={onToggleButton}>Sebete at</Button>
+      <h1>Divan</h1>
+      <Button onClick={onToggleButton}>Sebete at</Button>
+      <h1>Shkaf</h1>
+      <Button onClick={onToggleButton}>Sebete at</Button>
     </>
   );
 }
